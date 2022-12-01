7bbc51336d2303b4c3d7f1e2a44fab0d909dea2b
kevin chang <kevinchang@kkstream.com>
2022-12-01 15:23:03 -0600

    setup env

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(
     description='Slime Volleyball Gym Environment',
     packages=['slimevolleygym'],
     install_requires=[
-        'gym>=0.9.4',
+        'gym==0.19.0',
         'numpy>=1.13.0',
-        'opencv-python>=3.4.2.0'
+        'opencv-python>=3.4.2.0',
+        'stable-baselines[mpi]==2.10.0',
+        'tensorflow==1.14.0'
     ]
 )
